e14ee9d889b1445c26e8a06c2ba857440e83ee79
sweethr100 <sweethr100@gmail.com>
2024-11-27 21:19:44 +0900

    m -> rn, l -> I

---
+++ main.py
@@ -0,0 +1,14 @@
+def brakecode(user_code):
+    output=""
+    for i in user_code:
+        if i == "m":
+            output+="rn"
+        elif i == "l":
+            output+="I"
+        else:
+            output+=i
+    return output
+
+if __name__ == "__main__":
+    user_code = input("니 코드 입력 : ")
+    print(brakecode(user_code))          
